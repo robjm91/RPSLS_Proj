e48735f7626f34f0db24e4f99e065e40edb4bbca
Robert J Magaña <robjmagana@gmail.com>
2020-11-01 19:22:12 -0800

    created methods for computer/player classes

--- rpsls-app.js
+++ rpsls-app.js
@@ -28,18 +28,43 @@ class Human extends Player {
   constructor() {
     super();
   }
- chooseGesture () {
+
+ askName() {
+   var playerName;
+   playerName = prompt("Hello,\n What is your name?");
+   this.name = playerName;
+ }
+
+ chooseGesture() {
    let playerChoice = prompt ("Choose a Gesture!\nrock(1), paper(2), scissors(3), lizard(4), spock(5)");
-   
+   if(playerChoice < 1 || playerChoice > 5) {
+     console.log("Please Choose A Valid Gesture Option" + playerChoice);
+   }
+   else{
+     this.choice = this.gestureObjects[playerChoice];
+   }
  }
 }
+
 class Computer extends Player { 
   constructor() {
     super ();
+    this.name = "AI";
   }
+  
+  randomNumber() {
+    let randomNumber = Math.floor(Math.random() * 5);
+    return randomNumber;
+  }
+
  chooseGesture() {
+   let computerGestureChoice = this.gestures[this.null()];
+   this.choice = computerGestureChoice;
  }
+
 }
+
+
 
 
 
