4c6d35250e4182e4b6925b0aa781ab839c68579d
Robert J Magaña <robjmagana@gmail.com>
2020-11-02 18:27:31 -0800

    Restructured methods in game class: created function dispalyRules and createPlayers

--- rpsls-app.js
+++ rpsls-app.js
@@ -1,118 +1,141 @@
 "use strict";
 
 
-// class Game {
-//   constructor() {
-//     this.playerOne = new Player(); 
-//     this.playerTwo = new Player();
-//   }
+class Game {
+  constructor() {
+    this.playerOne = new Player(); 
+    this.playerTwo = new Player();
+  }
 
-//   runGame() {
-//     alert("Welcome to RPSLS!\nPress the 'OK' button to continue.");
-//     console.log("The rules are simple:\n");
-//     console.log("Rock Crushes Scissors\nScissors Cuts Paper\nPaper Covers Rock\nRock Crushes Lizard\nLizard Poisons Spock\nLizard Poisons Spock\nScissors Decapitates Lizard\nLizard Eats Paper\nPaper Disproves Spock\nand Spock Vaporizes Rock...HAVE FUN!!! :)");
+  displayRules(){
+    alert("Welcome to RPSLS!\nPress the 'OK' button to continue.");
+    console.log("The rules are simple:\n");
+    console.log("Rock Crushes Scissors\nScissors Cuts Paper\nPaper Covers Rock\nRock Crushes Lizard\nLizard Poisons Spock\nLizard Poisons Spock\nScissors Decapitates Lizard\nLizard Eats Paper\nPaper Disproves Spock\nand Spock Vaporizes Rock...HAVE FUN!!! :)");
 
-//     let playerGameMode = prompt("Which Game Mode? For Player vs AI press [1], For Player vs Player press [2]");
-//     let playerGameModeSelect = parseInt();
-//     if(playerGameModeSelect === 1) {
-//       console.log("Game Mode: Player vs AI");
-//       return chooseGesture();
+  }
 
-//     }
-//     else if(playerGameModeSelect === 2) {
-//       console.log("Game Mode: Player vs Player");
-//       return chooseGesture();
-//     }
-  
-//   }
+  createPlayers(){
+    let playerGameMode = prompt("Which Game Mode? For Player vs AI press [1], For Player vs Player press [2]");
+    let playerGameModeSelect = parseInt(playerGameMode);
 
+    if(playerGameModeSelect === 1) {
+      console.log("\nGame Mode: Player vs AI");
 
-// }
+      this.playerOne = new Human();
+      this.playerTwo = new Computer();
 
+    }
+    else if(playerGameModeSelect === 2) {
+      console.log("\nGame Mode: Player vs Player");
 
-
-// class Player { 
-//   constructor() {
-//     this.name = "";
-//     this.score = 0; 
-//     this.choice = "";
-//   }
-//   chooseGesture () {
-//     console.log("Overide This Method");
-//   }
-// }
+      this.playerOne = new Human();
+      this.playerTwo = new Human();
+     
 
 
-// class Gesture extends Player {
-//   constructor() {
-//     super ();
-//     this.gesture = ["rock", "paper", "scissors", "lizard", "spock"];
-//   }
-//   chooseGesture() {
-//     let playerChoice = prompt ("Choose a Gesture!\n rock(1), paper(2), scissors(3), lizard(4), spock(5)");
-//     if(playerChoice < 1 || playerChoice > 5) {
-//       console.log("Please Choose A Valid Gesture Option" + playerChoice);
-//     }
-//     else{
-//       this.choice = this.gestureObjects[playerChoice];
-//     }
-//   }
-// }
 
+    }
 
-// class Human extends Player { 
-//   constructor() {
-//     super();
-//   }
+  }
 
-//  askName() {
-//    var playerName;
-//    playerName = prompt("Hello,\n What is your name?");
-//    this.name = playerName;
-//  }
+  runGame() {
+   
+    this.displayRules();
+    this.createPlayers();
 
- 
-// }
-// let playerOne = new Player();
-// console.log(playerOne);
+    //Start gameplay
+    //Both players choose gesture
+    //Compare gestures, determine winner
+    //repeat
 
-// class Computer extends Player { 
-//   constructor() {
-//     super ();
-//     this.name = "AI";
-//   }
   
 
-//   randomNumber() {
-//     let randomNumber = Math.floor(Math.random() * 5);
-//     return randomNumber;
-//   }
 
-//  chooseGesture() {
-//    let computerGestureChoice = this.gestures[this.null()];//this.null temporary(TODO)
-//    this.choice = computerGestureChoice;
-//  }
-
-// }
-
-// let game = new Game();
-// game.runGame();
+  
+  }
 
 
-//constructor function
-function Player (name, score, choice) {
-this.name = name;
-  this.score = score;
-  this.choice = choice;
 }
 
-function choiceSelect(){
-  var result;
-  result = prompt("rock[1], paper[2], scissors[3], lizard[4], spock[5]");
-  return result;
+
+
+
+
+
+
+
+class Player { 
+  constructor() {
+    this.name = "";
+    this.score = 0; 
+    this.choice = "";
+    this.gestureObjects = ["rock", "paper", "scissors", "lizard", "spock"];//Will conflict with line 95
+  }
+  chooseGesture () {
+    console.log("Overide This Method");
+  }
+}
+
+class Human extends Player { 
+  constructor() {
+    super();
+  }
+  
+  askName() {
+    var playerName;
+    playerName = prompt("Hello,\n What is your name?");
+    this.name = playerName;
+  }
+  chooseGesture() {
+    let playerChoice = prompt ("Choose a Gesture!\n rock(1), paper(2), scissors(3), lizard(4), spock(5)");
+    if(playerChoice < 1 || playerChoice > 5) {
+      console.log("Please Choose A Valid Gesture Option" + playerChoice);
+    }
+    else{
+      this.choice = this.gestureObjects[playerChoice];//conflicts with line 72, might need -1 because index of rock is[0] in gestureobjects array
+    }
+  }
+  
   
 }
-choiceSelect();
+
+
+
+
+
+
+
+
+
+
+let playerOne = new Player();
+console.log(playerOne);
+
+
+
+class Computer extends Player { 
+  constructor() {
+    super ();
+    this.name = "AI";
+  }
+  
+
+  randomNumber() {
+    let randomNumber = Math.floor(Math.random() * 5);
+    return randomNumber;
+  }
+
+ chooseGesture() {
+   let computerGestureChoice = this.gestures[this.null()];//this.null temporary(TODO)
+   this.choice = computerGestureChoice;
+ }
+
+}
+
+let game = new Game();
+game.runGame();
+
+
 
 
 
